48ea60e023e50f90df424ebfcb96654309db08df
Gavrikov Ivan <gipard6@gmail.com>
2022-02-06 22:04:15 +0500

    Чётные числа от 1 до N

---
+++ C#/Exercize_004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число");
+string? numberum = Console.ReadLine();
+int number = int.Parse(numberum);
+int i = 1;
+
+while (i <= number) {
+    if (i %2 == 0) {
+    Console.Write($"{i} ");
+    i++;
+    }
+    else {
+       i++;
+    }
+}
