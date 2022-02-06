3c89b03656fd3805a7a78ff22558b4259e92db44
Gavrikov Ivan <gipard6@gmail.com>
2022-02-06 21:52:55 +0500

    Является ли число чётным

---
+++ C#/Exercize_003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число четным
+
+Console.WriteLine("Введите число");
+string? numberum = Console.ReadLine();
+int number = int.Parse(numberum);
+
+if (number %2 == 0)
+    Console.WriteLine($"{number} четное число");
+    else
+        Console.WriteLine($"{number} нечетное число");
