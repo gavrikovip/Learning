30c6de6d1269b4fb23b5e85b38af62bce5975acb
Gavrikov Ivan <gipard6@gmail.com>
2022-02-06 21:47:47 +0500

    Найти максимальное из трех чисел

---
+++ C#/Exercize_002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Введите первое число");
+string? numberFirst = Console.ReadLine();
+int numberOne = int.Parse(numberFirst);
+
+Console.WriteLine("Введите второе число");
+string? numberSecond = Console.ReadLine();
+int numberTwo = int.Parse(numberSecond);
+
+Console.WriteLine("Введите третье число");
+string? numberThird = Console.ReadLine();
+int numberThree = int.Parse(numberThird);
+
+int max = numberOne;
+
+if (numberOne >= numberTwo)
+    max = numberOne;
+    else if (numberTwo >= numberThree)
+        max = numberTwo;
+        else
+            max = numberThree;
+
+Console.WriteLine($"{max} максимальное число");
