51edd4af295592fb5edc79f6f3d9d5adf3e6e1c7
Gavrikov Ivan <gipard6@gmail.com>
2022-02-06 21:35:36 +0500

    Большее и меньшее числа

--- C#/Exercize_001/Program.cs
+++ C#/Exercize_001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Даны два числа. Показать большее и меньшее число
+
+
+Console.WriteLine("Введите первое число");
+string? numberFirst = Console.ReadLine();
+int numberOne = int.Parse(numberFirst);
+
+Console.WriteLine("Введите второе число");
+string? numberSecond = Console.ReadLine();
+int numberTwo = int.Parse(numberSecond);
+
+if (numberOne == numberTwo) 
+    Console.Write("Числа одинаковые");
+
+else if (numberOne > numberTwo) 
+    Console.WriteLine($"{numberOne} большее, {numberTwo} меньшее");
+
+else Console.WriteLine($"{numberTwo} большее, {numberOne} меньшее");
