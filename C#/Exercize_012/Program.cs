f7e45b3f2a4ac7f5094fa19d3134461a8f4039c5
Gavrikov Ivan <gipard6@gmail.com>
2022-02-10 00:14:14 +0500

    Вторая цифра трехзначного числа. Удаление.

---
+++ C#/Exercize_012/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру трёхзначного числа.
+
+int GetNumber()
+{
+    return new Random().Next(100, 1000);
+}
+int number = GetNumber();
+Console.WriteLine(number);
+
+int firstDigital = number / 100;
+int thirdDigital = number % 10;
+int finalNumber = firstDigital * 10 + thirdDigital;
+
+Console.WriteLine(finalNumber);
