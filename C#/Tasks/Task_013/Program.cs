9f5eccf3393a2118f82f207e7050c408bd472e6a
Gavrikov Ivan <gipard6@gmail.com>
2022-02-09 23:23:27 +0500

    Кратно ли число заданному. Если нет, то показать остаток

---
+++ C#/Tasks/Task_013/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному. Если нет, то вывести остаток
+
+int GetNumber()
+{
+    return new Random().Next(20, 100);
+}
+
+int constNumber = GetNumber();
+int number = GetNumber();
+//Console.WriteLine(number);
+int ostatok = number % constNumber;
+
+if (ostatok == 0 )
+    Console.WriteLine($"{number} кратно {constNumber}");
+else
+    Console.WriteLine($"Остаток от деления {number} на {constNumber} равен {ostatok}");
