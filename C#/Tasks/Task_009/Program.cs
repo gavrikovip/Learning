42d783d8f00e308a44857f1b35f05c14a84fe648
Gavrikov Ivan <gipard6@gmail.com>
2022-02-09 22:29:10 +0500

    Показать последнюю цифру трехзначного числа

---
+++ C#/Tasks/Task_009/Program.cs
@@ -0,0 +1,24 @@
+﻿//Показать последнюю цифру трехзначного числа
+
+int GetLastDigit(int number)
+{
+    return number % 10;
+}
+
+int GetIntegerFromConsole(int min, int max)
+{
+    int result = 0;
+    bool isError = true;
+    while (isError)
+    {
+        Console.WriteLine($"Введите целое число от {min} по {max}:");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out result)) isError = result < min || result > max;
+    }
+    return result;
+
+}
+
+int number = GetIntegerFromConsole(100, 999);
+int lasDigit = GetLastDigit(number);
+Console.WriteLine(lasDigit);
