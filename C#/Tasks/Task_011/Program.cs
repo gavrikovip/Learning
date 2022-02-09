67eba58e7946d93a040a1b9335251fe991a106cc
Gavrikov Ivan <gipard6@gmail.com>
2022-02-09 23:08:24 +0500

    Наибольшая цифра числа из отрезка

---
+++ C#/Tasks/Task_011/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int GetRandomNumber()
+{
+    return new Random().Next(10, 100);
+}
+
+int number = GetRandomNumber();
+Console.WriteLine(number);
+
+int firstNumber = number / 10;
+int secondNumber = number % 10;
+
+if (firstNumber > secondNumber) 
+    Console.WriteLine($"Большее из цифр {firstNumber}");
+else 
+    Console.WriteLine($"Большее из цифр {secondNumber}");
