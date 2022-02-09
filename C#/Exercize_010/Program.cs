ce05e8bebc566deaaa8f76c6d338ccc2801361e4
Gavrikov Ivan <gipard6@gmail.com>
2022-02-10 00:00:41 +0500

    Вторая цифра трехзначного числа

---
+++ C#/Exercize_010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать вторую цифру трёхзначного числа.
+
+int GetNumber()
+{
+    return new Random().Next(100, 1000);
+}
+int number = GetNumber();
+Console.WriteLine(number);
+//142 = 1*10(2) + 4*10(1) + 2*10(0)
+
+int SecondDigital = (number / 10) % 10;
+Console.WriteLine(SecondDigital);
