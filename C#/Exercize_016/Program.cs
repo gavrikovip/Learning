2dffab98f14de32f2806e6e76995c9a821047247
Gavrikov Ivan <gipard6@gmail.com>
2022-02-10 22:46:06 +0500

    Является ли номер дня выходным?

---
+++ C#/Exercize_016/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня выходным.
+
+int Nomer()
+{
+    return new Random().Next(1,32);
+}
+
+int nomer = Nomer();
+
+if (nomer % 7 == 6 || nomer % 7 == 0)
+    Console.WriteLine($"Номер {nomer} выпадает на выходной день.");
+else
+{
+    Console.WriteLine($"Номер {nomer} не выпадает на выходной день.");
+}
