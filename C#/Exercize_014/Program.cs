6c412929570ff0f3f2d34cd9f2d059bec46d8588
Gavrikov Ivan <gipard6@gmail.com>
2022-02-10 20:59:01 +0500

    Найти третью цифру числа, есть есть

---
+++ C#/Exercize_014/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что ее нет
+
+int GetNumber()
+{
+    return new Random().Next(1, 999999999);
+}
+
+int number = GetNumber();
+
+Console.WriteLine(number);
+
+string mynumber = number.ToString();
+
+if (mynumber.Length >=3)
+    Console.WriteLine(mynumber[2]);
+else
+    Console.WriteLine("Нет третьей цифры в числе");
