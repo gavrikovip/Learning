3b9ea46bfe5f9b4a1bd37289b1f32135a1e7f30c
Gavrikov Ivan <gipard6@gmail.com>
2022-02-13 23:09:48 +0500

    Кубы чисел от 1 до N

---
+++ C#/Exercize_024/Program.cs
@@ -0,0 +1,24 @@
+﻿//Найти кубы чисел от 1 до N
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"{i+1} в кубе = {arr[i]}");
+    }
+}
+
+int[] InitArray(int number)
+{
+    int [] array = new int[number]; 
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = (i+1)*(i+1)*(i+1);
+    }
+    return array;
+}
+
+int coin = new Random().Next(1,50);
+
+int [] a = InitArray(coin);
+PrintArray(a);
