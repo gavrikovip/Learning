3186a325a75d81717026e02b81fcc4e9082a2f07
Gavrikov Ivan <gipard6@gmail.com>
2022-02-17 00:14:28 +0500

    Массив из 8 элементов 0 или 1

---
+++ C#/Exercize_032/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] ArrayInit()
+{
+    int[] arr = new int[8] {0,0,0,0,0,0,0,0};
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = GetNumber();
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(int item in array)
+    {
+        Console.Write(item + " ");
+    }
+}
+
+int GetNumber()
+{
+    int number = new Random().Next(0, 2);
+    return number;
+}
+
+int[] arr = ArrayInit();
+PrintArray(arr);
