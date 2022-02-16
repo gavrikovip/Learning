91d65208617ad94cec0a05e3ac41d938663e193d
Gavrikov Ivan <gipard6@gmail.com>
2022-02-16 23:14:24 +0500

    Сумма цифр в числе

---
+++ C#/Exercize_028/Program.cs
@@ -0,0 +1,27 @@
+﻿// Подсчитать сумму цифр в числе
+
+// 123 1+2+3
+// 123%10 = 3
+// 123/10 = 12
+// 12%10 = 2
+// 12/10 = 1
+// 1%10 = 1
+// 1/10 = 0
+int GetNumber()
+{
+    int number = new Random().Next(1,99999999);
+    return number;
+}
+
+int number = GetNumber();
+Console.WriteLine($"Выпало число {number}");
+int ostatokTen = 0;
+
+while (number != 0)
+{
+    ostatokTen = number % 10 + ostatokTen;
+    int celoeTen = number / 10;
+    number = celoeTen;
+}
+
+Console.WriteLine($"Сумма цифр в числе = {ostatokTen}");
