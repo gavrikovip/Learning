76e2585784168eb346e0c63ef0dcdf1fb34830c5
Gavrikov Ivan <gipard6@gmail.com>
2022-02-16 22:52:56 +0500

    Число А в степени B

---
+++ C#/Exercize_026/Program.cs
@@ -0,0 +1,18 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int numberA = GetNumber("Введите число А: ");
+int numberB = GetNumber("Введите число B: ");
+int numberFirstA = numberA;
+for (int i = 1; i < numberB; i++)
+{
+    numberA *= numberFirstA; 
+}
+
+Console.WriteLine($"Число {numberFirstA} в степени {numberB} = {numberA}");
