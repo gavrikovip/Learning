b01a74f3beaf1b5b40936322cc7d75e987e6736f
Gavrikov Ivan <gipard6@gmail.com>
2022-02-21 21:32:38 +0500

    Сумма чисел массива на нечетной позиции

---
+++ C#/Exercize_038/Program.cs
@@ -0,0 +1,46 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции.
+
+int GetNumber()
+{
+    Console.WriteLine("Введите количество элементов массива:");
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
+
+int[] InitArray(int number)
+{
+    int[] arr = new int[number];
+    Random randomNumber = new Random();
+    for (int i = 0; i < number; i++)
+    {
+        arr[i] = randomNumber.Next(1, 51);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void PrintSumOfArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++) // считаем, что позиция числа начинается с 1 а не с 0. Иначе меняем условаие на !=0.
+    {
+        if (i % 2 == 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    Console.WriteLine($"Сумма = {sum}");
+}
+
+int getInt = GetNumber();
+int[] arr = InitArray(getInt);
+PrintArray(arr);
+Console.WriteLine();
+PrintSumOfArray(arr);
