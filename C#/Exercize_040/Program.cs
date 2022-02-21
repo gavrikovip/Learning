84f0d9b9d46833617c8b76d515e72ec99ae278f4
Gavrikov Ivan <gipard6@gmail.com>
2022-02-21 21:34:11 +0500

    Разница между максимальным и минимальным вещественными числами в массиве

---
+++ C#/Exercize_040/Program.cs
@@ -0,0 +1,28 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+
+double []arr = new double[]{12, -2, 3, 4, -5, 69.2, 3, 2, 0};
+
+void MaxMinArray()
+{
+    double max = arr[0];
+    double min = arr[0];
+    foreach(double item in arr)
+    {
+        if (item >= max)
+        {
+            max = item;
+        }
+        else
+        {
+            if (item <= min)
+            {
+                min = item;
+            }
+        }
+    }
+    Console.WriteLine($"Максимальный элемент {max}, Минимальный элемент {min}");
+    double difference = max - min;
+    Console.WriteLine($"Разница межде числами = {difference}");
+}
+
+MaxMinArray();
