ae6bac9de4f26dd354ec7056f28f4961481e51dc
Gavrikov Ivan <gipard6@gmail.com>
2022-02-24 17:20:54 +0500

    Найти точку пересечения двух прямых.

---
+++ C#/Exercize_044/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+int b1 = 4;
+int b2 = 1;
+int k1 = 5;
+int k2 = 2;
+
+// k1x + b1 = k2x + b2
+// x = (b1 - b2) / (k2 - k1)
+// 
+
+if (k2 == k1)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+else
+{
+    double x = (b1 - b2) / (k2 - k1);
+    Console.Write($"x = {x}, ");
+    double y = k1*x + b1;
+    Console.Write($"y = {y}");
+}
