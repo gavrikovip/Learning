559a3e069f148edb95f4b3376d34021fda1cd50c
Gavrikov Ivan <gipard6@gmail.com>
2022-02-24 21:37:54 +0500

    Масштабирование фигуры, точнее множитель координат

---
+++ C#/Exercize_046/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу масштабирования фигуры
+using System.Text;
+
+string strValue = "(0,2) (2,3) (1,0) (2,2)";
+Console.WriteLine("Введите множитель");
+double k = double.Parse(Console.ReadLine());
+
+// Убираем скобки и запятые
+strValue = strValue.Replace("(", "").Replace(")", "").Replace(",", " ");
+
+
+// Создание массива на базе строки
+string[] strArray = strValue.Split();
+
+// Вывод массива без лишних символов
+foreach (var item in strArray)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+
+//Использование StringBuilder для сохранения оперативной памяти при создании строки с множителем
+StringBuilder newString = new StringBuilder();
+
+for (int i = 0; i < strArray.Length - 1; i = i + 2)
+{
+    double x = k * int.Parse(strArray[i]);
+    double y = k * int.Parse(strArray[i + 1]);
+    newString.Append($"({x}, {y}) ");
+}
+
+Console.WriteLine(newString);
+
