dd55f839702e76b0f45ba0ed57f344af67c7f8b6
Gavrikov Ivan <gipard6@gmail.com>
2022-02-27 13:34:13 +0500

    Замена элементов с четными индексами на квадраты

---
+++ C#/Exercize_052/Program.cs
@@ -0,0 +1,50 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты.
+
+Random numberOfArray = new Random();
+
+// Заполнение двумерного массива
+int[,] FillArray(int m, int k)
+{
+    int[,] arr = new int[m, k];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            arr[i, j] = numberOfArray.Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+// Печать массива
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Возведение четных индексов в квадрат
+int[,] SquadArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                arr[i, j] = arr[i, j] * arr[i, j];
+        }
+    }
+    return arr;
+}
+
+int[,] array = FillArray(4, 4);
+PrintArray(array);
+Console.WriteLine();
+array = SquadArray(array);
+PrintArray(array);
