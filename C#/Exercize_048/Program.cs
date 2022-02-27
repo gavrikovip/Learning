681856886649a69336a4a80b50c26d11ccb16514
Gavrikov Ivan <gipard6@gmail.com>
2022-02-27 13:31:39 +0500

    Двумерный массив заполненный целыми числами

---
+++ C#/Exercize_048/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами.
+Random numberArray = new Random();
+
+void FillArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = numberArray.Next(1, 100);
+            Console.Write($"{arr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int m = numberArray.Next(1, 5);
+int n = numberArray.Next(1, 5);
+
+Console.WriteLine($"m = {m} ");
+Console.WriteLine($"n = {n} ");
+FillArray(m, n);
