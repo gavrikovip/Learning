0d749d2256186c0d83c3d9d38c1eb1fbba08aa22
Gavrikov Ivan <gipard6@gmail.com>
2022-05-15 16:14:07 +0500

    Task 35 N натуральных чисел без одного

---
+++ Python/Task35.py
@@ -0,0 +1,37 @@
+# В файле находится N натуральных чисел, записанных через пробел.
+# Среди чисел не хватает одного, чтобы выполнялось условие A[i]-1 = A[i-1].
+# Найдите это число.
+
+from ast import If
+
+
+with open('Nчисел.txt', 'r') as data:
+    s = list(data.readline().split())
+print(s)
+
+# Преобразование строк в числа
+a = []
+for i in s:
+    try:
+        a.append(int(i))
+    except ValueError:
+        pass
+print(a)
+
+# Вывод найденного числа
+
+
+def num(a):
+    for j in a:
+        try:
+            if(a[j] - 1 == a[j-1]):
+                j += 1
+            else:
+                return print(f'{j+1}')
+            if j == len(a):
+                print('Все числа на месте')
+        except ValueError:
+            pass
+
+
+num(a)
